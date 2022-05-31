f518ecbcce71d4d1515c8788b809bc9cf009e782
madaniata <98869214+madaniata@users.noreply.github.com>
2022-05-31 09:25:40 +0700

    Create main.R

---
+++ .github/workflows/main.R
@@ -0,0 +1,29 @@
+# Create Twitter token API --------------------------------
+
+library(rtweet)
+
+cuacabogor_token <- rtweet::create_token(
+  app = "cuacabogor",
+  consumer_key = 'ePf756VGtEJTHeHZgYzrHMlNp',
+  consumer_secret = 'g2mi5REpuWi5yrMmGR7w5slVcyCjWQi01pSP7cxlUbnjcnrfeJ',
+  access_token = '1489125681510248453-0locTYPSc8kDgfWbPEf6SJbxmlf3Qj',
+  access_secret = 'ukAcEMBKERxwhcAys5vXGr3iOFzXkZt3tyh9oxC1kBila'
+)
+
+## batas ==================================================================
+
+pht = getSourceEditorContext()$path
+pht = gsub("/main.R", "", pht)
+
+setwd(pht)
+source('cuacabogor.R')
+
+# Menyambungkan MongoDB Database ke R ------------------------
+
+# This is the connection_string. You can get the exact url from your MongoDB cluster screen
+library(mongolite)
+connection_string = 'mongodb+srv://madaniata:tamadania17@MDS.kqfqm.mongodb.net/sample_training'
+cuacajabar_collection = mongo(collection="cuacaBogor",
+                              db="UAS_MDS",
+                              url=connection_string)
+cuacajabar_collection$insert(dataSiap)
