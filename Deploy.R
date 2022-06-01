f460c71dff23c1c854e1bc7f6a044bc643e05338
madaniata <98869214+madaniata@users.noreply.github.com>
2022-06-01 12:03:58 +0700

    Update Deploy.R

--- Deploy.R
+++ Deploy.R
@@ -10,6 +10,19 @@ cuacabogor_token <- rtweet::create_token(
   access_secret = 'ukAcEMBKERxwhcAys5vXGr3iOFzXkZt3tyh9oxC1kBila'
 )
 
+## batas ==================================================================
+
+Scraping_Data <- function(con, twitter_token) {
+library(rvest)
+urlbogor <- "https://www.bmkg.go.id/cuaca/prakiraan-cuaca-indonesia.bmkg?Prov=10&NamaProv=Jawa%20Barat"
+tabelbogor <- read_html(urljabar)
+databogor <- html_table(tabeljabar)
+databogor <- as.data.frame(datajabar)
+databogor <- datajabar[-2,]
+databogor <- datajabar[-2,]
+
+library(dplyr)
+dataSiap <- databogor[16, ]
 # Menyambungkan MongoDB Database ke R ------------------------
 
 # This is the connection_string. You can get the exact url from your MongoDB cluster screen
@@ -18,16 +31,31 @@ connection_string = 'mongodb+srv://madaniata:tamadania17@MDS.kqfqm.mongodb.net/s
 cuacajabar_collection = mongo(collection="cuacaBogor",
                               db="UAS_MDS",
                               url=connection_string)
-cuacajabar_collection$insert(dataSiap)
+cuacajabar_collection$insert(databogor)
+}
 
-## batas ==================================================================
+Tweet_Posting <- function(con, twitter_token) {
+# Hashtag
+hashtag <- "CuacaBogor"
 
-#pht = getSourceEditorContext()$path
-#pht = gsub("/Deploy.R", "", pht)
+# Build the status message
+status_details <- paste0("Prediksi cuaca Bogor esok hari ",
+                         "\n",
+                         "Pagi: ", dataSiap[8],
+                         "\n",
+                         "Siang: ", dataSiap[9],
+                         "\n",
+                         "Malam: ", dataSiap[10],
+                         "\n",
+                         "Dini hari: ", dataSiap[11],
+                         " #",hashtag)
 
-setwd(getwd())
-source('ScrapingData.R')
-source('TweetPosting.R')
+## Post the image to Twitter
+rtweet::post_tweet(
+  status = status_details,
+  token = cuacajabar_token
+)
+}
 
 Scraping_Data(con, twitter_token)
 Tweet_Posting(con, twitter_token)
