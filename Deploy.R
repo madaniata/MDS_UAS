a6b6e6c9c4cf8994913e74a77bcb97659133acfb
madaniata <98869214+madaniata@users.noreply.github.com>
2022-06-25 09:14:47 +0700

    Update Deploy.R

--- Deploy.R
+++ Deploy.R
@@ -7,10 +7,10 @@ library(mongolite)
 
 cuacabogor_token <- rtweet::create_token(
   app = "cuacabogor",
-  consumer_key = 'ePf756VGtEJTHeHZgYzrHMlNp',
-  consumer_secret = 'g2mi5REpuWi5yrMmGR7w5slVcyCjWQi01pSP7cxlUbnjcnrfeJ',
-  access_token = '1489125681510248453-0locTYPSc8kDgfWbPEf6SJbxmlf3Qj',
-  access_secret = 'ukAcEMBKERxwhcAys5vXGr3iOFzXkZt3tyh9oxC1kBila'
+  consumer_key =    Sys.getenv("TWITTER_CONSUMER_API_KEY"),
+  consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
+  access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
+  access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 )
 
 ## batas ==================================================================
@@ -25,7 +25,7 @@ dataSiap <- databogor[16, ]
 # Menyambungkan MongoDB Database ke R ------------------------
 
 # This is the connection_string. You can get the exact url from your MongoDB cluster screen
-connection_string = 'mongodb+srv://madaniata:tamadania17@MDS.kqfqm.mongodb.net/sample_training'
+connection_string = Sys.getenv("CONNECTION_STRING")
 cuacabogor_collection = mongo(collection="cuacaBogor",
                               db="UAS_MDS",
                               url=connection_string)
