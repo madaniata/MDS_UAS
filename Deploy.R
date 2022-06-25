9c88abe525bfccf48012b49f5b994ea5a3d696e7
madaniata <98869214+madaniata@users.noreply.github.com>
2022-06-25 21:49:11 +0700

    Update Deploy.R

--- Deploy.R
+++ Deploy.R
@@ -6,7 +6,7 @@ library(mongolite)
 # Create Twitter token API --------------------------------
 
 cuacabogor_token <- rtweet::create_token(
-  app = "cuaca_bogor",
+  app = "madaniata17",
   consumer_key = Sys.getenv("TWITTER_CONSUMER_API_KEY"),
   consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
   access_token = Sys.getenv("TWITTER_ACCESS_TOKEN"),
